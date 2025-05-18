477fd2a883bd3b3cfd0ce3c37f46e357d5cd72db
AI Anam <beenaanam9@gmail.com>
2025-05-18 17:36:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// JavaScript 1st Asssignment
+
+//  Q1 greet website visitor
+alert(`"Hello, >(ɔ◔‿◔)ɔ ♥ Welcome to my Quotes Page"`);
+
+// Q2 display message
+alert("Error! Please enter a valid password");
+
+// Q3 display foolowing message
+// alert(` "Welcome to JS Land..." '\n' "Happy Coding" `)
+alert("Welcome to JS Land..." + '\n'+ "Happy Coding ");
+
+// Q4 display message in sequence
+alert("Welcome to JS Land");
+
+alert("Happy Coding!" + '\n' + '\n' + " ☐ Prevent this page from creating additional dialogs" );
+
+// Q5 generate this message through my web browser's console
+alert("Hellow... I can run JS through my web browser's console")
+
+//  Q6
+alert("What's your name ,Age , Qualification ")
+let name ="Beena Anum";
+let age = 25;
+let edu = "Graduation";
+confirm("Your name is :" + name +'\n'+ "Your age :"+ age + '\n'+ "Your qualification: " +edu)
+alert("Thankyou")
+// Q7 I have checked script tage in following section 1) head, 2) body (before html) 3)body (Inside html) 4) body (After html) 
+// all  are result is same
